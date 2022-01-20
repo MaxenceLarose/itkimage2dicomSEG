f00258ea5628885593fb0a70043afaacfe6388f4
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2022-01-20 17:01:33 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='itkimage2dicomseg',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://gitlab.chudequebec.ca/MaxenceLarose/itkimage2dicomseg',
+    license="Apache License 2.0",
+    author='Maxence Larose',
+    author_email="maxence.larose.1@ulaval.ca",
+    description="Simplify the conversion of segmentation in commonly used research file formats like NRRD and NIfTI "
+                "into the standardized DICOM-SEG format. ",
+    install_requires=[
+        "grpm-uid",
+        "pydicom",
+        "pydicom-seg",
+    ]
+)
