12690ce7ecb9c1723683213ccd403c4f7ec61d8f
Maxence Larose <46930386+MaxenceLarose@users.noreply.github.com>
2022-01-26 10:30:06 -0500

    Update __init__.py

--- itkimage2dicomseg/__init__.py
+++ itkimage2dicomseg/__init__.py
@@ -1,7 +1,7 @@
 from .dicom_seg.dicom_seg_writer import DicomSEGWriter
 from .paths_manager.path_generator import PathGenerator
-from .patient_folder_structure.structure import structure_patients_folder
-from .patient_folder_structure.destructure import destructure_patients_folder
+from .patients_folder_structure.structure import structure_patients_folder
+from .patients_folder_structure.destructure import destructure_patients_folder
 
 __author__ = "Maxence Larose"
 __version__ = "0.0.1"
